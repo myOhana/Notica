c8fd04122e892c81bdfd3408d2c1ea3ca434046d
shahpreetk <preet25@somaiya.edu>
2020-05-31 20:06:49 +0530

    Updates

---
+++ precache-manifest.38926c7bf00d7a5e37d51a1b92712765.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a4012a9475e7ffd44fe20658ea5e7d0",
+    "url": "/Notica/index.html"
+  },
+  {
+    "revision": "77770829934b943fb549",
+    "url": "/Notica/static/css/main.5ec5c1da.chunk.css"
+  },
+  {
+    "revision": "7887855b41705ad9d093",
+    "url": "/Notica/static/js/2.ce6703c5.chunk.js"
+  },
+  {
+    "revision": "c6b13bca67f1afcf5cd35a3d59d7258d",
+    "url": "/Notica/static/js/2.ce6703c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77770829934b943fb549",
+    "url": "/Notica/static/js/main.ee269bd6.chunk.js"
+  },
+  {
+    "revision": "078a6a7a6e7e4d6ae092",
+    "url": "/Notica/static/js/runtime-main.729fbcb1.js"
+  }
+]);
